f8307db4106b2c45ca91c0c99854f9ac08b2fa6e
wassim <82286951+Wassim-Rached@users.noreply.github.com>
2025-01-13 20:17:29 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment: Environment = {
   termsAndConditionsUrl: 'https://example.com',
 
   // central authentication service
-  cas: 'https://cas-server.azurewebsites.net',
+  cas: 'https://cas-server-2.azurewebsites.net',
   // identity and access control service
-  ics: 'https://ics-server.azurewebsites.net'
+  ics: 'https://ics-server-2.azurewebsites.net'
 };
